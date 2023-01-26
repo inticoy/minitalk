1508ff8b9151b0e5caa32c7c940f3ecc4098ebbe
inticoy <inticoy0406@gmail.com>
2023-01-26 20:56:59 +0900

    server simple version

--- src/server/server.c
+++ src/server/server.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:24:34 by gyoon             #+#    #+#             */
-/*   Updated: 2023/01/26 20:29:36 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/01/26 20:56:45 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,19 @@ typedef struct sigaction	t_sigaction;
 
 void	handler(int sig)
 {
-	if (sig == SIGUSR1)
-		ft_printf("catch USR1\n");
-	else if (sig == SIGUSR2)
-		ft_printf("catch USR2\n");
+	static char	ch;
+	static char	bit;
 
+	ch <<= 1;
+	if (sig == SIGUSR1)
+		ch |= 0x01;
+	bit++;
+	if (bit == 8)
+	{
+		write(1, &ch, 1);
+		ch = 0;
+		bit = 0;
+	}
 }
 
 int	main(void)
@@ -31,16 +39,14 @@ int	main(void)
 	pid_t		pid;
 
 	pid = getpid();
-	printf("Server Process ID : %d\n", pid);
-
+	ft_printf("Server Process ID : %d\n", pid);
+	ft_printf("%x", 'A');
 	sigact.sa_handler = handler;
 	sigemptyset(&sigact.sa_mask);
-	sigact.sa_flags |= SA_RESTART;
+	sigact.sa_flags = SA_RESTART;
 	sigaction(SIGUSR1, &sigact, FT_NULL);
 	sigaction(SIGUSR2, &sigact, FT_NULL);
 	while (1)
 		;
 	return (0);
 }
-
-
