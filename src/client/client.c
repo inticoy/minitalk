9000659b8a8df2774098ac0568a0ce8e81747da2
inticoy <inticoy0406@gmail.com>
2023-01-27 17:16:25 +0900

    server and client works properly with unicode but sometimes miss bits

--- src/client/client.c
+++ src/client/client.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:24:31 by gyoon             #+#    #+#             */
-/*   Updated: 2023/01/27 15:01:11 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/01/27 17:09:50 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,19 @@
 #include <signal.h>
 #include <unistd.h>
 
-void	handler(int sig)
-{
-	static char	ch;
-	static char	bit;
+volatile sig_atomic_t	ack;
 
-	ch <<= 1;
+void	handler(int sig, siginfo_t *info, void *v)
+{
+	(void) info;
+	(void) v;
+	ft_printf("sig : %d\n", sig);
 	if (sig == SIGUSR1)
-		ch |= 0x01;
-	bit++;
-	if (bit == 8)
-	{
-		write(1, &ch, 1);
-		ch = 0;
-		bit = 0;
-	}
+		ack = 1;
+	else if (sig == SIGUSR2)
+		ack = 0;
+	else
+		ack = -1;
 }
 
 int	main(int argc, char **argv)
@@ -40,13 +38,16 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 		raise_exception("execution must be './client [pid] [msg]'\n");
 	pid = ft_atoi(argv[1]);
-	sigact.sa_handler = handler;
+
+	sigact.sa_sigaction = handler;
 	sigemptyset(&sigact.sa_mask);
-	sigact.sa_mask |= SIGUSR1 | SIGUSR2;
+	sigaddset(&sigact.sa_mask, SIGUSR1);
+	sigaddset(&sigact.sa_mask, SIGUSR2);
 	sigact.sa_flags |= SA_RESTART | SA_SIGINFO;
 	sigaction(SIGUSR1, &sigact, FT_NULL);
 	sigaction(SIGUSR2, &sigact, FT_NULL);
 
+	ack = -1;
 	unsigned int i = 0;
 	while (i < ft_strlen(argv[2]))
 	{
@@ -55,15 +56,28 @@ int	main(int argc, char **argv)
 		while (j < 8)
 		{
 			if (c & 0x01)
+			{
 				kill(pid, SIGUSR1);
+				while (1)
+					if (ack == 1)
+						break ;
+				ack = -1;
+			}
 			else
+			{
 				kill(pid, SIGUSR2);
+				while (1)
+					if (ack == 0)
+						break ;
+				ack = -1;
+			}
 			c >>= 1;
 			j++;
-			usleep(1000);
 		}
 		i++;
 	}
+
+	ft_printf("%d\n", getpid());
 	ft_printf("%d\n", ft_atoi(argv[1]));
 	ft_printf("%s\n", argv[2]);
 	return (0);
--- src/server/server.c
+++ src/server/server.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:24:34 by gyoon             #+#    #+#             */
-/*   Updated: 2023/01/27 15:01:17 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/01/27 17:15:32 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,25 @@
 
 typedef struct sigaction	t_sigaction;
 
-void	handler(int sig)
+void	handler(int sig, siginfo_t *info, void *v)
 {
 	static char	ch;
 	char		test;
 	static char	bit;
 	static char count;
 
-	test = 0;
+	(void) v;
+	test = 0x00;
 	if (sig == SIGUSR1)
+	{
 		test = 0x01 << bit;
+		kill(info->si_pid, SIGUSR1);
+	}
+	else
+	{
+		test = 0x00;
+		kill(info->si_pid, SIGUSR2);
+	}
 	ch |= test;
 	bit++;
 	if (bit == 8)
@@ -35,7 +44,6 @@ void	handler(int sig)
 		bit = 0;
 	}
 	count++;
-	//ft_printf("%d\n", count);
 }
 
 int	main(void)
@@ -45,13 +53,14 @@ int	main(void)
 
 	pid = getpid();
 	ft_printf("Server Process ID : %d\n", pid);
-	ft_printf("%x", 'A');
-	sigact.sa_handler = handler;
+
+	sigact.sa_sigaction = handler;
 	sigemptyset(&sigact.sa_mask);
 	sigact.sa_mask |= SIGUSR1 | SIGUSR2;
 	sigact.sa_flags |= SA_RESTART | SA_SIGINFO;
 	sigaction(SIGUSR1, &sigact, FT_NULL);
 	sigaction(SIGUSR2, &sigact, FT_NULL);
+
 	while (1)
 		;
 	return (0);
