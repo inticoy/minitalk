338ee8e5604933ea14553f485b571ef362351a51
inticoy <inticoy0406@gmail.com>
2023-01-29 21:54:15 +0900

    server and client

--- src/client/client.c
+++ src/client/client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:24:31 by gyoon             #+#    #+#             */
-/*   Updated: 2023/01/27 17:09:50 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/01/29 21:16:03 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	handler(int sig, siginfo_t *info, void *v)
 {
 	(void) info;
 	(void) v;
-	ft_printf("sig : %d\n", sig);
 	if (sig == SIGUSR1)
 		ack = 1;
 	else if (sig == SIGUSR2)
@@ -38,15 +37,13 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 		raise_exception("execution must be './client [pid] [msg]'\n");
 	pid = ft_atoi(argv[1]);
-
 	sigact.sa_sigaction = handler;
 	sigemptyset(&sigact.sa_mask);
 	sigaddset(&sigact.sa_mask, SIGUSR1);
 	sigaddset(&sigact.sa_mask, SIGUSR2);
-	sigact.sa_flags |= SA_RESTART | SA_SIGINFO;
+	sigact.sa_flags = SA_RESTART | SA_SIGINFO;
 	sigaction(SIGUSR1, &sigact, FT_NULL);
 	sigaction(SIGUSR2, &sigact, FT_NULL);
-
 	ack = -1;
 	unsigned int i = 0;
 	while (i < ft_strlen(argv[2]))
@@ -55,30 +52,24 @@ int	main(int argc, char **argv)
 		char c = argv[2][i];
 		while (j < 8)
 		{
+			usleep(15);
 			if (c & 0x01)
 			{
 				kill(pid, SIGUSR1);
-				while (1)
-					if (ack == 1)
-						break ;
-				ack = -1;
+				while (ack != 1)
+					;
 			}
 			else
 			{
 				kill(pid, SIGUSR2);
-				while (1)
-					if (ack == 0)
-						break ;
-				ack = -1;
+				while (ack != 0)
+					;
 			}
+			ack = -1;
 			c >>= 1;
 			j++;
 		}
 		i++;
 	}
-
-	ft_printf("%d\n", getpid());
-	ft_printf("%d\n", ft_atoi(argv[1]));
-	ft_printf("%s\n", argv[2]);
 	return (0);
 }
--- src/server/server.c
+++ src/server/server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:24:34 by gyoon             #+#    #+#             */
-/*   Updated: 2023/01/27 17:15:32 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/01/29 21:44:12 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,39 @@
 #include "libft.h"
 #include <signal.h>
 
-typedef struct sigaction	t_sigaction;
-
 void	handler(int sig, siginfo_t *info, void *v)
 {
 	static char	ch;
-	char		test;
 	static char	bit;
-	static char count;
 
 	(void) v;
-	test = 0x00;
 	if (sig == SIGUSR1)
-	{
-		test = 0x01 << bit;
-		kill(info->si_pid, SIGUSR1);
-	}
+		ch |= 0x01 << bit;
 	else
+		ch |= 0x00;
+	kill(info->si_pid, sig);
+	if (++bit == 8)
 	{
-		test = 0x00;
-		kill(info->si_pid, SIGUSR2);
-	}
-	ch |= test;
-	bit++;
-	if (bit == 8)
-	{
-		write(1, &ch, 1);
+		ft_putchar_fd(ch, 1);
 		ch = 0;
 		bit = 0;
 	}
-	count++;
 }
 
 int	main(void)
 {
-	t_sigaction	sigact;
-	pid_t		pid;
+	struct sigaction	sigact;
+	pid_t				pid;
 
 	pid = getpid();
 	ft_printf("Server Process ID : %d\n", pid);
-
 	sigact.sa_sigaction = handler;
 	sigemptyset(&sigact.sa_mask);
-	sigact.sa_mask |= SIGUSR1 | SIGUSR2;
-	sigact.sa_flags |= SA_RESTART | SA_SIGINFO;
+	sigaddset(&sigact.sa_mask, SIGUSR1);
+	sigaddset(&sigact.sa_mask, SIGUSR2);
+	sigact.sa_flags = SA_RESTART | SA_SIGINFO;
 	sigaction(SIGUSR1, &sigact, FT_NULL);
 	sigaction(SIGUSR2, &sigact, FT_NULL);
-
 	while (1)
 		;
 	return (0);
